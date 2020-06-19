a12b96be34c69a2302af155be0e4c4cde6edaa12
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-19 12:32:34 +0300

    added controller for post rout

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
 exports.getPosts = (req,res,next)=>{
     res.status(200).json({
         posts:[{title: "first post" , content: "yep that is first"}]
     })
 }
 
+exports.createPost = (req,res,next) =>{
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: "post was created successfuly",
+        post:{id: uuidv4(), title: title, content: content}
+    })
+}
