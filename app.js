4c2af5d66cc651b5c411ad224f993016138dfbf8
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-19 12:31:38 +0300

    added bodyparsing middleware + setting headers

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const feedRoutes = require('./routes/feed.js');
 
+
+app.use(bodyParser.json());     //application/json
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type','Authorization');
+    next();
+})
+
 app.use('/feed',feedRoutes);
 
 
-
+console.log("success");
 app.listen(8080);
