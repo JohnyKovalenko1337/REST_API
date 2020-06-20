6e386b918812cc578e1ed5f48ead7bae5ff16863
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-20 19:45:44 +0300

    added dummy post

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,7 +2,17 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.getPosts = (req,res,next)=>{
     res.status(200).json({
-        posts:[{title: "first post" , content: "yep that is first"}]
+        posts:[
+            {
+                _id:'1',
+                title: "first post" ,
+                content: "yep that is first",
+                imageUrl:'./image/Kchau.jpg',
+                creator: {
+                    name:"Yevhen"
+                },
+                date: new Date()
+            }]
     })
 }
 
@@ -11,6 +21,14 @@ exports.createPost = (req,res,next) =>{
     const content = req.body.content;
     res.status(201).json({
         message: "post was created successfuly",
-        post:{id: uuidv4(), title: title, content: content}
+        post:{
+            id: uuidv4(),
+            title: title, 
+            content: content,
+            creator:{
+                name:"Yevhen"
+            },
+            createdAt: new Date()
+        }
     })
 }
