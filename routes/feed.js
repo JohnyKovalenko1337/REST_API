29185e0c324027bcdec34291fe3d652d4d92db7d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-21 13:32:48 +0300

    added singePost router + validation for post rout

--- routes/feed.js
+++ routes/feed.js
@@ -7,6 +7,15 @@ const feedController = require('../controllers/feed');
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
-router.post('/create', feedController.createPost);
 
+router.post('/post', [
+    body('title')
+        .trim()
+        .isLength({min:5}),
+    body('content')
+        .trim()
+        .isLength({min:5})
+], feedController.createPost);
+
+router.get('/post/:postId', feedController.getPost);
 module.exports = router;
