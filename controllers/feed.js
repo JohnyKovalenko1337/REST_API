45e40f1c9608580b406a53f343e3a32b4635eb20
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-21 22:16:37 +0300

    updated createPost module image handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -24,12 +24,18 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if(!req.file){
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'image/Kchau.jpg',
+    imageUrl: imageUrl,
     creator: { name: 'Yevhen' }
   });
   post
