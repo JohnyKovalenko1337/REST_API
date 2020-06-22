c8dbfbfdaf97c1459f2a19396cf9098d4ef74fda
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-22 11:55:09 +0300

    added update pit rout for editting

--- routes/feed.js
+++ routes/feed.js
@@ -18,4 +18,14 @@ router.post('/post', [
 ], feedController.createPost);
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId',[
+    body('title')
+        .trim()
+        .isLength({min:5}),
+    body('content')
+        .trim()
+        .isLength({min:5})
+],feedController.updatePost);
+
 module.exports = router;
