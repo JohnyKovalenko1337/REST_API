353bc1a80650dce744c8b964add7140c6dccf0fb
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-22 18:13:36 +0300

    new user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    post:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
+});
+
+module.export = mongoose.model('User', userSchema);
