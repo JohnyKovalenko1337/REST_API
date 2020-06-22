090eff726dab079da68aa783d91e3de0b06a3675
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-22 23:13:45 +0300

    added signup controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 exports.signup = (req,res,next)=>{
     const errors = validationResult(req);
@@ -13,4 +14,51 @@ exports.signup = (req,res,next)=>{
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
+    bcrypt.hash(password,12)
+    .then(hashedPw=>{
+        const user = new User({
+            name: name,
+            email: email,
+            password: hashedPw
+        });
+        return user.save()
+    })
+    .then(result=>{
+        res.status(201).json({message:"successfuly added new user",userId: result._id})
+    })
+    .catch(err=>{
+        if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          next(err);
+    });
+}
+
+exports.login = (req,res,next)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({email:email})
+    .then(user=>{
+        if(!user){
+            const error = new Error('Couldnt be found');
+            error.statusCode = 401;
+            throw error;    
+        }
+        loadedUser = user;
+        return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual)=>{
+        if(!isEqual){
+            const error = new Error('Password is wrong');
+            error.statusCode = 401;
+            throw error; 
+        }
+    })
+    .catch(err=>{
+        if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          next(err);
+    });
 }
