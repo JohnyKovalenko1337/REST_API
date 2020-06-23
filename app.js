25503ad33a847399efebfa7983256175ac38e504
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-23 12:11:28 +0300

    fixed header error

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/image', express.static(path.join(__dirname, 'image')));
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type','Authorization');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
     next();
 })
 
