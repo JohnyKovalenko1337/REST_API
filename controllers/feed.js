00423047b98fff4965586f2cfc635ea7d68096c9
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-23 15:59:16 +0300

    added deleting relation btw post and user model

--- controllers/feed.js
+++ controllers/feed.js
@@ -175,6 +175,13 @@ exports.deletePost = (req,res,next)=>{
     return Post.findByIdAndRemove(postId)          
   })
   .then(result=>{
+    return  User.findById(req.userId)
+  })
+  .then(user=>{
+    user.posts.pull(postId);
+    return user.save();
+  })
+  .then(result=>{
     res.status(200).json({message:'Post deleted successfuly',})
   })
   .catch(err=>{
