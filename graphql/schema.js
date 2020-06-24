db9b22d8e2871229e36c9d27336e63c6b2e5296b
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-24 19:19:48 +0300

    added models in schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,10 +1,40 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+type Post{
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User
+    createdAt: String!
+    updatedAt: String!
+}
+
+type User{
+    _id: ID!
+    email:String!
+    name:String!
+    password: String!
+    status: String!
+    posts: [Post!]
+}
+
+input UserInputData{
+    email:String!
+    name:String!
+    password: String!
+}
+
 type RootQuery{
-    hello: String
+    hello:String!
+}
+
+type RootMutation{
+    createUser(userInput:UserInputData): User!
 }
 schema{
-    query: RootQuery
+    query:RootQuery
+    mutation:RootMutation
 }
 `)
