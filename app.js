bae02ed1a1f3f0406b4e1e220a173a09b543d2cc
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-25 19:12:20 +0300

    fixed bug with graphql

--- app.js
+++ app.js
@@ -46,6 +46,9 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    if(req.method === "OPTIONS"){
+        return res.sendStatus(200);
+    }
     next();
 })
 
@@ -54,7 +57,7 @@ app.use('/graphql', graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    formatError(err){
+    customFormatErrorFn(err){
         if(!err.originalError){
             return err;
         }
