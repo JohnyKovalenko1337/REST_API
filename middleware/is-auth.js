18231efdab126cc718ac0ac06bddc4e74949edc5
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-26 22:26:12 +0300

    upgrated for frontend logic

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,26 +1,26 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req,res,next)=>{
+module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
-    if(!authHeader){
-        const error = new Error ('Not Autenticated');
-        error.statusCode = 401;
-        throw error;
+    if (!authHeader) {
+        req.isAuth = false;
+        return next();
     }
     const token = authHeader.split(' ')[1];
     let decodedToken;
-    try{
-        decodedToken = jwt.verify(token,'somesupersecret')
+    try {
+        decodedToken = jwt.verify(token, 'somesupersecret')
     }
-    catch(err){
+    catch (err) {
         err.statusCode = 500;
         throw err;
     }
-    if(!decodedToken){
-        const error = new Error ('Not Autenticated');
+    if (!decodedToken) {
+        const error = new Error('Not Autenticated');
         error.statusCode = 401;
         throw error;
     }
     req.userId = decodedToken.userId;
+    req.isAuth = true;
     next();
 }
