872275f4deb3b1d94254c79046382169818b9227
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-26 22:27:05 +0300

    added createPost mutation

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,47 +1,59 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-type Post{
-    _id: ID!
-    title: String!
-    content: String!
-    imageUrl: String!
-    creator: User
-    createdAt: String!
-    updatedAt: String!
-}
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
 
-type User{
-    _id: ID!
-    email:String!
-    name:String!
-    password: String!
-    status: String!
-    posts: [Post!]
-}
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
 
-type AuthData{
-    token: String!
-    userId: String!
-}
+    type AuthData {
+        token: String!
+        userId: String!
+    }
 
-input UserInputData{
-    email:String!
-    name:String!
-    password: String!
-}
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
 
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
 
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
+    }
 
-type RootQuery{
-    login(email: String!, password: String!): AuthData!
-}
+    type RootQuery {
+        login(email: String!, password: String!): AuthData!
+        posts: PostData!
+    }
 
-type RootMutation{
-    createUser(userInput:UserInputData): User!
-}
-schema{
-    query:RootQuery
-    mutation:RootMutation
-}
-`)
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
