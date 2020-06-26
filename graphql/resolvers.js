71763a682f08629adcf5ac635ed9bd87e1730373
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-26 22:27:28 +0300

    fixed lots of login bugs

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,55 +1,96 @@
 const User = require('../models/user');
+const Post = require('../models/post');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const isAuth = require('../middleware/is-auth');
 module.exports = {
-    createUser : async function({userInput},req){
+    createUser: async function ({ userInput }, req) {
         //const email = args.userInput.email;
         const errors = [];
-        if(!validator.isEmail(userInput.email)){
-            errors.pust({message:'Email is invalid'})
+        if (!validator.isEmail(userInput.email)) {
+            errors.pust({ message: 'Email is invalid' })
         }
-        if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min:5})){
-            errors.push({message:'Enter longer password'})
+        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+            errors.push({ message: 'Enter longer password' })
         }
-        if(errors.length > 0){
+        if (errors.length > 0) {
             const error = new Error('Validation failed');
             error.data = errors;
             error.code = 422;
             throw error;
         }
-        const existingUser = await User.findOne({email:userInput.email})
-        if(existingUser){
+        const existingUser = await User.findOne({ email: userInput.email })
+        if (existingUser) {
             const error = new Error("User exists");
-            throw existingUser;
+            throw error;
         }
-        const hashedPsw = await bcrypt.hash(userInput.password,12);
+        const hashedPsw = await bcrypt.hash(userInput.password, 12);
         const user = new User({
             email: userInput.email,
             name: userInput.name,
-            password: userInput.password
+            password: hashedPsw
         });
         const createdUser = await user.save();
-        return {...createdUser._doc, _id:createdUser._id.toString()};
-    
-   },
-   login: async function({email, password}) {
-       const user = await User.find({email:email});
-       if(!user){
-           const error = new Error('User not found');
+        return { ...createdUser._doc, _id: createdUser._id.toString() };
+
+    },
+    login: async function ({ email, password }) {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            const error = new Error('User not found');
             error.code = 401;
             throw error;
-       }
-       const isEqual =await bcrypt.compare(password,user.password); 
-       if(!isEqual){
-           const error = new Error('password is incorrect');
-           error.code = 401;
-           throw error;
-       }
-       const token = jwt.sign({
-           userId:user._id.toString(),
-           email: email
-       }, 'somesupersecret',{expiresIn:'1h'});
-       return {token:token , userId: token.user._id.toString()};
-   }
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error('password is incorrect');
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email: user.email
+        }, 'somesupersecret', { expiresIn: '1h' });
+        return { token: token, userId: user._id.toString() };
+    },
+    createPost: async function ({ postInput }, req) {
+        if(!req.isAuth){
+            const error = new Error('Not autenticated');
+            error.code = 401;
+            throw error;
+        }
+        const errors = [];
+        if (validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, { min: 5 })) {
+            errors.push({ message: "title is not validated" });
+        }
+        if (validator.isEmpty(postInput.content) || !validator.isLength(postInput.content, { min: 5 })) {
+            errors.push({ message: "content is not validated" });
+        }
+        if (errors.length > 0) {
+            const error = new Error('Validation failed');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        const user = await new User.findById(req.userId);
+        if(!user){
+            const error = new Error('User is unknown');
+            error.code = 401;
+            throw error;
+        }
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl,
+            creator: user
+        });
+        const createdPost = await post.save();
+        return {
+            ...createdPost._doc,
+            _id: createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString()
+        };
+    }
 }
