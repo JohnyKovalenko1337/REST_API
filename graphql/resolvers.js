19356786407cfe96084598465a6299e96c820a75
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-27 14:29:31 +0300

    added pagination logic

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -54,73 +54,79 @@ module.exports = {
         }, 'somesupersecret', { expiresIn: '1h' });
         return { token: token, userId: user._id.toString() };
     },
-    createPost: async function({ postInput }, req) {
+    createPost: async function ({ postInput }, req) {
         if (!req.isAuth) {
-          const error = new Error('Not authenticated!');
-          error.code = 401;
-          throw error;
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
         }
         const errors = [];
         if (
-          validator.isEmpty(postInput.title) ||
-          !validator.isLength(postInput.title, { min: 5 })
+            validator.isEmpty(postInput.title) ||
+            !validator.isLength(postInput.title, { min: 5 })
         ) {
-          errors.push({ message: 'Title is invalid.' });
+            errors.push({ message: 'Title is invalid.' });
         }
         if (
-          validator.isEmpty(postInput.content) ||
-          !validator.isLength(postInput.content, { min: 5 })
+            validator.isEmpty(postInput.content) ||
+            !validator.isLength(postInput.content, { min: 5 })
         ) {
-          errors.push({ message: 'Content is invalid.' });
+            errors.push({ message: 'Content is invalid.' });
         }
         if (errors.length > 0) {
-          const error = new Error('Invalid input.');
-          error.data = errors;
-          error.code = 422;
-          throw error;
+            const error = new Error('Invalid input.');
+            error.data = errors;
+            error.code = 422;
+            throw error;
         }
         const user = await User.findById(req.userId);
         if (!user) {
-          const error = new Error('Invalid user.');
-          error.code = 401;
-          throw error;
+            const error = new Error('Invalid user.');
+            error.code = 401;
+            throw error;
         }
         const post = new Post({
-          title: postInput.title,
-          content: postInput.content,
-          imageUrl: postInput.imageUrl,
-          creator: user
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl,
+            creator: user
         });
         const createdPost = await post.save();
         user.posts.push(createdPost);
         await user.save();
         return {
-          ...createdPost._doc,
-          _id: createdPost._id.toString(),
-          createdAt: createdPost.createdAt.toISOString(),
-          updatedAt: createdPost.updatedAt.toISOString()
+            ...createdPost._doc,
+            _id: createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString()
         };
     },
-    posts: async function(args, req) {
+    posts: async function ({ page }, req) {
         if (!req.isAuth) {
-          const error = new Error('Not authenticated!');
-          error.code = 401;
-          throw error;
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
         }
+        if (!page) {
+            page = 1;
+        }
+        const perPage = 3;
         const totalPosts = await Post.find().countDocuments();
         const posts = await Post.find()
-          .sort({ createdAt: -1 })
-          .populate('creator');
+            .sort({ createdAt: -1 })
+            .skip( ( page - 1 ) * perPage)
+            .limit( perPage )
+            .populate('creator');
         return {
-          posts: posts.map(p => {       //ovewriting id createad at and updatedAt
-            return {
-              ...p._doc,
-              _id: p._id.toString(),    
-              createdAt: p.createdAt.toISOString(),
-              updatedAt: p.updatedAt.toISOString()
-            };
-          }),
-          totalPosts: totalPosts
+            posts: posts.map(p => {       //ovewriting id createad at and updatedAt
+                return {
+                    ...p._doc,
+                    _id: p._id.toString(),
+                    createdAt: p.createdAt.toISOString(),
+                    updatedAt: p.updatedAt.toISOString()
+                };
+            }),
+            totalPosts: totalPosts
         };
     }
 }
