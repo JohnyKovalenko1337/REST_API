4fd55d1e3e41f96888421e7fc07411ebc04098d1
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-28 10:39:48 +0300

    added clearImage func

---
+++ util/clear.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+const clearImage = filePath =>{
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err=>{
+      console.log(err);
+    })
+  };
+
+exports.clearImage = clearImage;
